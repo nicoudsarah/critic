a41d47544477a63d98b56d4641844349495e3208
sarah <sarah@sogilis.com>
2020-02-07 11:49:56 +0100

    [DEV] Critic: critic.json is now created with a minimal content

--- src/main/java/Critic.java
+++ src/main/java/Critic.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.NotDirectoryException;
 
@@ -24,5 +25,8 @@ public class Critic {
 
         File criticEmptyFile = new File("test/samples/EmptyRepository/critic.json");
         criticEmptyFile.createNewFile();
+        String outputContent = "{\n}";
+        FileOutputStream fileWriter = new FileOutputStream(criticEmptyFile);
+        fileWriter.write(outputContent.getBytes());
     }
 }
--- src/test/java/CriticTest.java
+++ src/test/java/CriticTest.java
@@ -3,7 +3,9 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.nio.file.Files;
 import java.nio.file.NotDirectoryException;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -26,6 +28,7 @@ public class CriticTest {
     void EmptyRepositoryCase() {
         String emptyRepositoryPath = "test/samples/EmptyRepository";
         String outputCriticJSON = "test/samples/EmptyRepository/critic.json";
+        String expectedCriticJSON = "test/samples/EmptyRepository/expected.critic.json";
 
         RemoveFile(outputCriticJSON);
 
@@ -33,8 +36,21 @@ public class CriticTest {
         Assertions.assertDoesNotThrow(() -> critic.evaluate()) ;
         File criticPathtoFile = new File(outputCriticJSON);
         assertTrue(criticPathtoFile.exists());
+        File expectedCriticFile = new File(expectedCriticJSON);
+        assertTrue(FilesContentsAreEquals(expectedCriticJSON, outputCriticJSON));
+    }
 
-        RemoveFile(outputCriticJSON);
+    private boolean FilesContentsAreEquals(String path1, String path2) {
+        File f1 = new File(path1);
+        File f2 = new File(path2);
+        try {
+            byte[] content1 = Files.readAllBytes(f1.toPath());
+            byte[] content2 = Files.readAllBytes(f2.toPath());
+            return Arrays.equals(content1, content2);
+        } catch (Exception e)
+        {
+            return false;
+        }
     }
 
     private void RemoveFile(String fileName) {
